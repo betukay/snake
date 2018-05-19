93dbb70a9eeeb6e6225ee27bf56542ca5cc5f7e5
Betu <bkay210@gmail.com>
2018-05-19 18:21:41 -0400

    first commit

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var s;
+var scl = 20;
+var food;
+
+function setup() {
+  createCanvas(600, 600);
+  s = new Snake();
+  frameRate(10);
+  food = createVector(random(width), random(height))
+}
+
+function draw() {
+  background(51);
+  s.update();
+  s.show();
+
+  fill(255, 0, 100);
+}
+
+function keyPressed(){
+  if(keyCode === UP_ARROW) {
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
+  }
+}
